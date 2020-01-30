619852654a645d940a1497b29cd7fd554f1a8dcd
ltx <ltx0720@gmail.com>
2020-01-30 17:57:10 +0800

    倒序打印链表

---
+++ twoP/twoS/T6.java
@@ -0,0 +1,38 @@
+package twoP.twoS;
+
+/**
+ * @Author ltx
+ * @Date 15:18 2020/1/30
+ *
+ * 倒叙打印链表。运用栈
+ */
+public class T6 {
+
+    //倒序输出链表
+    public static void print(Node<Integer> head){
+        if (head == null){
+            return;
+        }
+        print(head.next);
+        System.out.println(head.data);
+    }
+
+    public static Node<Integer> generic(int[] array){
+        Node<Integer> head = new Node<>(array[0]);
+        Node<Integer> bak = head;
+
+        for (int i=1; i<array.length; i++){
+            bak.next = new Node<>(array[i]);
+            bak = bak.next;
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1,2,3,4,5,6,7};
+
+        Node<Integer> node = generic(array);
+        print(node);
+    }
+}
