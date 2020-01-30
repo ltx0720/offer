744900b9a0897b3339242d71fc1d7b77b5c09566
ltx <ltx0720@gmail.com>
2020-01-30 17:56:34 +0800

    数组中重复数字

---
+++ twoP/twoS/T3.java
@@ -0,0 +1,13 @@
+package twoP.twoS;
+
+/**
+ * @Author ltx 
+ * @Date 17:17 2020/1/29
+ */
+public class T3 {
+
+    public static void main(String[] args) {
+        int[][] ints = {{1, 2, 3}, {4, 5, 6}};
+        System.out.println(ints[0][2]);
+    }
+}
