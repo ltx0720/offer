63e709c142de83c1b95c10b081022d9f34813675
ltx <ltx0720@gmail.com>
2020-01-30 17:55:54 +0800

    Tree

---
+++ twoP/twoS/Tree.java
@@ -0,0 +1,17 @@
+package twoP.twoS;
+
+/**
+ * @Author ltx
+ * @Date 15:53 2020/1/30
+ */
+public class Tree<T> {
+    public Tree<T> left;
+    public Tree<T> right;
+    public T data;
+
+    public Tree(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
